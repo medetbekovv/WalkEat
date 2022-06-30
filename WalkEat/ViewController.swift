76d7576b987631da04501064eacc39bf43526e7b
user <user@users-MacBook-Pro-2.local>
2022-06-30 21:33:56 +0600

    init app

---
+++ WalkEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalkEat
+//
+//  Created by user on 6/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
